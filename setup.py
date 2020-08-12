16057a56097625fc7f8efc3df26f89e84d88a390
Ant <anthony.hillairet@gmail.com>
2020-08-12 14:00:20 -0700

    upgrade version to 0.2-rc2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='SPyLib',
-    version='0.1',
+    version='0.2-rc2',
     packages=['spylib'],
     license='TBD',
     long_description=open('README.md').read(),
